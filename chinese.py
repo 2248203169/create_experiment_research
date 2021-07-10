2450f66c3b114867fb0ba0c5bae84a1aec3ffbef
2248203169 <83643832+2248203169@users.noreply.github.com>
2021-07-10 11:03:25 +0800

    Delete chinese.py

--- chinese.py
+++
@@ -1,7 +0,0 @@
-with open("D:/创新设计/H.txt","r",encoding='utf-8') as f:
-    s = f.read()
-    result = "".join(i for i in s if ord(i) > 256)
-    f.close()
-with open("D:/创新设计/yellow.txt",'w',encoding='utf-8') as f:
-    f.write(result)
-    f.close()
